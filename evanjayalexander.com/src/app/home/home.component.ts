b0946d8b2047f14a4b4cfe0ca9bc488827df0e3e
Evan Alexander <evanalexander012@gmail.com>
2025-05-09 20:48:00 -0600

    Added font awesome and animated background

--- evanjayalexander.com/src/app/home/home.component.ts
+++ evanjayalexander.com/src/app/home/home.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'home-component',
   standalone: true,
-  imports: [MatCardModule, MatListModule],
+  imports: [MatCardModule, MatListModule, FontAwesomeModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit {
+  faLinkedin = faLinkedin;
+  
   ngOnInit(): void {
       
   }
