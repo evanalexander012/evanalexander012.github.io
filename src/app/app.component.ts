7711d3a955fc7b4b90494f1a24018e6cb76bc61f
Evan Alexander <evanalexander012@gmail.com>
2026-05-28 09:56:29 -0600

    Added owl animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { ResumeComponent } from './resume/resume.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { OwlCompanionComponent } from './owl-companion/owl-companion.component';
 
 interface navButtons {
   [name: string] : { display: string, status: boolean }
@@ -16,7 +17,7 @@ interface navButtons {
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MatButtonModule, NgIf, NgFor, KeyValuePipe, HomeComponent, AboutComponent, ResumeComponent, ProjectsComponent, FontAwesomeModule],
+  imports: [MatButtonModule, NgIf, NgFor, KeyValuePipe, HomeComponent, AboutComponent, ResumeComponent, ProjectsComponent, OwlCompanionComponent, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
